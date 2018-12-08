d3c0c8809c980457dc9ff2f6357bb5cb0842f938
Luis-D <luisnestordl@hotmail.com>
2018-12-08 04:03:08 -0400

    Day1 loop fixed

--- Day1/Second.lua
+++ Day1/Second.lua
@@ -1,47 +1,30 @@
 -- Advent of Code 2018. Day 1, part 2.
 -- Luis Delgado.
--- This code works with the example, but it enters in an very long loop with the puzzle input (Data.txt)
 
 Data_File="Data.txt";
 local File = io.open (Data_File,"r");
 
+Acc = 0;
+Mem = {};
 Data = {};
-a = 0;
-e = 0;
-i=-1;
-o = 0;
-u = 0;
-flag = 0;
 
 for line in io.lines(Data_File) do 
-	i=i+1;
-	Data[i] = tonumber(line);
-  end
-
-Mem = {};
-Mem[0] = 0;
-
-while (flag == 0) do
-	o=u;	
-	u = o+1;
-	Mem[u] = Mem[o] + Data[a];
-	--print(Mem[o] .. " + " .. Data[a] .. " = " .. Mem[u]);
-
-	e=0;
-	while (e<u) do
-		--print(Mem[u] .. " == " .. Mem[e]) ;
-		if (Mem[u] == Mem[e]) then
-			flag=1;
-			print(Mem[u]);
-			break;
-		end
-		e=e+1;
-	end
-
-	a=a+1;
-	if(a>i) then 
-		a=0;
-	end
+	table.insert(Data, tonumber(line));
 end
 
-print(Mem[u]);
+flag = false
+
+while flag == false do
+    for k,v in pairs(Data) do
+	Acc = Acc+v
+
+	if(Mem[Acc]) then
+	    flag=true
+	    break;
+	end
+
+	Mem[Acc] = true;
+    end
+end
+
+print(Acc)
